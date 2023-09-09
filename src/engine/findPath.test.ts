192690b36b6f2f82d5ea27a960759fbb84e09ea5
Rene Hangstrup Møller <rhmoller@gmail.com>
2023-09-09 09:28:47 +0200

    fix: check if cell is in closed set before adding tyo open set, to avoid infinite loop in path finding

--- src/engine/findPath.test.ts
+++ src/engine/findPath.test.ts
@@ -20,7 +20,7 @@ describe("pathfinding", () => {
 
   it("should not find a path", () => {
     const map = new TileMap(5, 5);
-    const isFree = (p: Point) => map.getTile(p.x, p.y) === 0;
+    const isFree = (p: Point) => (p.x === 0 && p.y === 0) || map.getTile(p.x, p.y) === 0;
     map.setTile(0, 1, 1);
     map.setTile(1, 0, 1);
     const path = findPath(isFree, { x: 0, y: 0 }, { x: 4, y: 4 });
--- src/engine/findPath.ts
+++ src/engine/findPath.ts
@@ -36,16 +36,20 @@ export function findPath(
     const current = Object.values(open).reduce((a, b) => (a.f < b.f ? a : b));
     delete open[`${current.x},${current.y}`];
     closed[`${current.x},${current.y}`] = current;
+    if (current.x === target.x && current.y === target.y) {
+      break;
+    }
     for (let i = 0; i < fourDirections.length; i++) {
-      const pos = { x: current.x + fourDirections[i].x, y: current.y + fourDirections[i].y };
       const { x, y } = fourDirections[i];
+      const pos = { x: current.x + x, y: current.y + y };
       if (x === 0 && y === 0) continue;
       if (!isFree(pos)) continue;
       const g = current.g + (x === 0 || y === 0 ? 10 : 14);
       const h = hDist(pos, target);
       const f = g + h;
-      const existing = open[`${pos.x},${pos.y}`];
-      if (existing === undefined || existing.f > f) {
+      const existingOpen = open[`${pos.x},${pos.y}`];
+      const existingClosed = closed[`${pos.x},${pos.y}`];
+      if (existingClosed === undefined && (existingOpen === undefined || existingOpen.f > f)) {
         open[`${pos.x},${pos.y}`] = {
           ...pos,
           g,
